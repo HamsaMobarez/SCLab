ffa5446905cc87dde337df9e1703ef685776d24f
engibahaa <engiibahaa@gmail.com>
2019-12-18 18:26:08 +0200

    Enjii committed

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hamsa");
+            Console.WriteLine("Enjii");
         }
     }
 }
